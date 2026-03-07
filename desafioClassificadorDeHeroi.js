424ea3aa6e107925f6dbeec0c191e22df51160e4
matheusscarin <mscarinmedeiros@gmail.com>
2026-03-06 21:46:42 -0300

    Solution for challenge one

---
+++ desafioClassificadorDeHeroi.js
@@ -0,0 +1,30 @@
+let heroi = [
+    ["Felipao", 25000],
+    ["Matheus", 10]
+]
+
+for (i = 0; i < heroi.length; i++){
+    let nomeHeroi = heroi[i][0]
+    let xp = heroi[i][1]
+    let nivelHeroi = ""
+
+if (xp <= 1000){
+    nivelHeroi = "Ferro"    
+} else if (xp >= 1001 && xp <= 2000){
+    nivelHeroi = "Bronze"
+} else if (xp >= 2001 && xp <= 5000){
+    nivelHeroi = "Prata"
+} else if (xp >= 5001 && xp <= 7000){
+    nivelHeroi = "Ouro"
+} else if (xp >= 7001 && xp <= 8000){
+    nivelHeroi = "Platina"
+} else if (xp >= 8001 && xp <= 9000){
+    nivelHeroi = "Ascendente"
+} else if (xp >= 9001 && xp <= 10000){
+    nivelHeroi = "Imortal"
+} else if (xp>= 10001){
+    nivelHeroi = "Radiante"
+}
+
+console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivelHeroi}!`)
+}
