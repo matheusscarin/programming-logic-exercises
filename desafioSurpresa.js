a91ea46b3bb23e99181b65f44b98190a13c53c5f
matheusscarin <mscarinmedeiros@gmail.com>
2026-03-10 17:38:36 -0300

    Solution to the surprise challenge

---
+++ desafioSurpresa.js
@@ -0,0 +1,21 @@
+let mensagemCadastro = " Cadastrado com Sucesso"
+let pokemons = [
+    ["Poochyena", 2, "M", "13/13"],
+    ["Zigzagoon", 2, "F", "13/13"],
+    ["Dragonite", 5, "M", "25/25"],
+    ["Dragonite", 5, "F", "24/24"],
+    ["Dragonite", 5, "F", "24/24"],
+    ["Poochyena", 3, "F", "15/15"],
+    ["Hurmple", 2, "M", "7/14"]
+]
+
+console.log(
+    pokemons[0][0] + mensagemCadastro + "\n" +
+    pokemons[1][0] + mensagemCadastro + "\n" +     
+    pokemons[1][0] + mensagemCadastro + "\n" + 
+    pokemons[2][0] + mensagemCadastro + "\n" +
+    pokemons[3][0] + mensagemCadastro + "\n" +
+    pokemons[4][0] + mensagemCadastro + "\n" +
+    pokemons[5][0] + mensagemCadastro + "\n" +
+    pokemons[6][0] + mensagemCadastro
+)
