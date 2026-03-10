d7bf9e10f63e072bab6a106060134e085e1a8f4c
matheusscarin <mscarinmedeiros@gmail.com>
2026-03-09 21:40:01 -0300

    Solution for challenge two

---
+++ desafioCalculadoraDePartidasRankeadas.js
@@ -0,0 +1,26 @@
+// Os Jogadores são dividos em nome, número de vitórias e número de derrotas
+let resultado = partidasRankeadas("Felipão", 25000, 3)
+
+function partidasRankeadas(nomeDoHeroi, vitorias, derrotas){    
+    let saldoDeVitorias = vitorias - derrotas
+    let nivel = ""
+        if (saldoDeVitorias <= 10){
+            nivel = "Ferro"
+        } else if (saldoDeVitorias <= 20){
+            nivel = "Bronze"
+        } else if (saldoDeVitorias <= 50){
+            nivel = "Prata"
+        } else if (saldoDeVitorias <= 80){
+            nivel = "Ouro"
+        } else if (saldoDeVitorias <= 90){
+            nivel = "Diamante"
+        } else if (saldoDeVitorias <= 100){
+            nivel = "Lendário"
+        } else if (saldoDeVitorias >= 101){
+            nivel = "Imortal"
+        } 
+    return [saldoDeVitorias, nivel]
+}
+
+console.log(`O Herói tem saldo de vitórias de ${resultado[0]} e está no nível de ${resultado[1]}!`)
+
--- desafioClassificadorDeHeroi.js
+++ desafioClassificadorDeHeroi.js
@@ -1,5 +1,5 @@
 let heroi = [
-    ["Felipao", 25000],
+    ["Felipão", 25000],
     ["Matheus", 10]
 ]
 
@@ -10,17 +10,17 @@ for (i = 0; i < heroi.length; i++){
 
 if (xp <= 1000){
     nivelHeroi = "Ferro"    
-} else if (xp >= 1001 && xp <= 2000){
+} else if (xp <= 2000){
     nivelHeroi = "Bronze"
-} else if (xp >= 2001 && xp <= 5000){
+} else if (xp <= 5000){
     nivelHeroi = "Prata"
-} else if (xp >= 5001 && xp <= 7000){
+} else if (xp <= 7000){
     nivelHeroi = "Ouro"
-} else if (xp >= 7001 && xp <= 8000){
+} else if (xp <= 8000){
     nivelHeroi = "Platina"
-} else if (xp >= 8001 && xp <= 9000){
+} else if (xp <= 9000){
     nivelHeroi = "Ascendente"
-} else if (xp >= 9001 && xp <= 10000){
+} else if (xp <= 10000){
     nivelHeroi = "Imortal"
 } else if (xp>= 10001){
     nivelHeroi = "Radiante"
